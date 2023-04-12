5f4435f02699ea1c7b680561e8760c4f53deab09
Adrián Domínguez <adrian.dominguez1095@gmail.com>
2023-04-12 16:07:23 -0300

    agregado de carrito js

---
+++ js/paquetes.js
@@ -0,0 +1,6 @@
+const paquetes = [
+    {id:01, tipo:"Paquete", nombre:"Pc Oficina", precio: 37000, foto: "/photos/1g.jpg" },
+    {id:02, tipo:"Paquete", nombre:"Pc Gamer", precio: 70000, foto: "/photos/2g.png" },
+    {id:03, tipo:"Paquete", nombre:"Pc Gamer Elite", precio: 135000, foto: "/photos/3g.png" },
+    {id:04, tipo:"Paquete", nombre:"Pc Gamer Extreme", precio: 265000, foto: "/photos/4g.jpg" },
+];
--- js/script.js
+++ js/script.js
@@ -1,70 +1,107 @@
-let nombreIngresado=prompt("Bienvenido! Ingresa tu usuario")
-let contraseña=prompt("Ingresá tu contraseña")
 
-if ((nombreIngresado === "Adrian")&&(contraseña === "Dominguez15" )){
-    alert("Bienvenido a TECH912")
-} else {
-    alert("Lo siento, el usuario o contraseña son incorrectos. Intente nuevamente")
+let carrito = cargarCarrito();
+
+
+let sectionPaquetes = document.getElementById("section-paquetes");
+let sectionCarrito = document.getElementById("section-carrito");
+
+
+let totalCompra = document.createElement("div");
+totalCompra.innerHTML = "<h2>Total: $</h2>";
+sectionCarrito.appendChild(totalCompra);
+
+let montoTotalCompra = document.createElement("h2");
+montoTotalCompra.innerText = "0";
+totalCompra.appendChild(montoTotalCompra);
+
+let cantidadPaquetes = document.createElement("div");
+cantidadPaquetes.innerHTML = "<h3>Cantidad: </h3>";
+sectionCarrito.appendChild(cantidadPaquetes);
+
+let cantPaquetes = document.createElement("h3");
+cantPaquetes.innerText = "0";
+cantidadPaquetes.appendChild(cantPaquetes);
+
+let botonFinalizar = document.createElement("button");
+botonFinalizar.innerText = "Finalizar compra";
+sectionCarrito.appendChild(botonFinalizar);
+botonFinalizar.setAttribute("class", "boton");
+
+
+
+botonFinalizar.onclick = () => {
+    const precioFinal = montoTotalCompra.innerText;
+    // USO DE SWEET //
+    Swal.fire({
+        title: '¿Deseas finalizar tu compra?',
+        text: `Total a pagar: $${precioFinal}`,
+        showCancelButton: true,
+        confirmButtonColor: '#008f39',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Si',
+        cancelButtonText: 'No'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            Swal.fire(
+                'Compra confirmada',
+                '¡Que disfrutes!',
+                'success'
+            )
+            vaciarCarrito();
+        }
+    })
 }
 
-let resultadoBot = "51";
-for (let i = 0; i < 10; i++) {
-    preguntaBot = prompt("Si no eres un robot, completa el siguiente enunciado: 9+42");
-    if (resultadoBot === preguntaBot) {
-    alert("Es correcto")
-    break;
-} else {
-    alert("Intenta nuevamente")
-}
-}
 
-    let ComputadorasAmd= [
-        {
-            modelo: "AMD oficina",
-            precio: "58000",
-        },
-        {
-            modelo: "AMD Gamer",
-            precio: "85000",
-        }
-    ]
-    let ComputadorasIntel= [
-    {
-            modelo: "Intel oficina",
-            precio: "63000",
-        },
-        {
-            modelo: "INTEL gamer",
-            precio: "95000",
-        }
-    ]
 
-    function mostrarComputadorasAmd(){
-        console.log("Computadoras disponibles");
-        for (let i = 0; i < ComputadorasAmd.length; i++){
-            console.log(`${i + 1}. ${ComputadorasAmd[i].modelo} - $${ComputadorasAmd[i].precio}`);
-        }
-    }
-
-    function mostrarComputadorasIntel(){
-        console.log("Computadoras disponibles");
-        for (let i = 0; i < ComputadorasIntel.length; i++){
-            console.log(`${i + 1}. ${ComputadorasIntel[i].modelo} - $${ComputadorasIntel[i].precio}`);
-        }
-    }
-
-    let opcion =  prompt("Selecciona tu preferencia de PC. Escribe: ESC para salir \n1-AMD \n2-INTEL");
-    switch (opcion) {
-        case "1":
-            console.log("Seleccionaste AMD");
-            mostrarComputadorasAmd();
-            break;
-        case "2":
-            console.log("Seleccionaste INTEL");
-            mostrarComputadorasIntel();
-            break;
-        default:
-            alert("Error");
-            break;
-    } 
+for (const paquete of paquetes) {
+    let container = document.createElement("div");
+    container.setAttribute("class", "card-paquete");
+    container.innerHTML = ` <div class="img-container">
+                            <img src="${paquete.foto}" alt="${paquete.nombre}" class="img-paquete"/>
+                            </div>
+                            <div class="info-paquete">
+                            <p class="font">${paquete.nombre}</p>
+                            <strong class="font">$${paquete.precio}</strong>
+                            <button class="boton" id="btn${paquete.id}"> Agregar al carrito </button>
+                            </div>`;
+    sectionPaquetes.appendChild(container);
     
+    document.getElementById(`btn${paquete.id}`).onclick = () => agregarAlCarrito(`${paquete.id}`);
+}
+
+
+
+
+function agregarAlCarrito(id) {
+    carrito.push(paquetes.find(p => p.id == id));
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+    calcularTotalCarrito();
+}
+
+function calcularTotalCarrito() {
+    let total = 0;
+    for (const paquete of carrito) {
+        total += paquete.precio;
+    }
+    montoTotalCompra.innerText = total;
+    cantPaquetes.innerText = carrito.length;
+}
+
+function vaciarCarrito() {
+    montoTotalCompra.innerText = "0";
+    cantPaquetes.innerText = "0";
+    localStorage.clear();
+    carrito = [];
+}
+
+
+function cargarCarrito() {
+    let carrito = JSON.parse(localStorage.getItem("carrito"));
+    if (carrito == null) {
+        return [];
+    } else {
+        return carrito;
+    }
+}
+
